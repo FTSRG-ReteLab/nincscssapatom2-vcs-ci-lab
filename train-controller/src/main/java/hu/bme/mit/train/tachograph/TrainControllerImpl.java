5f73d52b1a9d869434cc049719cfac44e9ff73c0
Csaba Kigyosi <csabronc@gmail.com>
2018-03-05 06:12:14 -0800

    Gergo's feature

--- train-controller/src/main/java/hu/bme/mit/train/tachograph/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/tachograph/TrainControllerImpl.java
@@ -2,11 +2,16 @@ package hu.bme.mit.train.tachograph;
 
 import hu.bme.mit.train.interfaces.TrainController;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TrainControllerImpl implements TrainController {
 
+	public Timer timer = new Timer();
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+	boolean timerOn = false;
 
 	@Override
 	public void followSpeed() {
@@ -45,6 +50,16 @@ public class TrainControllerImpl implements TrainController {
 	@Override
 	public void setJoystickPosition(int joystickPosition) {
 		this.step = joystickPosition;
+		if (!timerOn) {
+			timerOn = true;
+
+			timer.scheduleAtFixedRate(new TimerTask() {
+				@Override
+				public void run() {
+					followSpeed();
+				}
+			},0,5*1000);
+		}
 	}
 
 }
